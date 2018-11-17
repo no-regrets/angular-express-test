325afc4ee0aa566ef9ca8a5ca67a9fa2c4c57429
Wes Kruger <wesleykruger@gmail.com>
2018-11-17 13:29:57 -0600

    updated jaws connection string

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,18 +1,23 @@
 const env = require('./env.js');
 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(env.database, env.username, env.password, {
-  host: env.host,
-  dialect: env.dialect,
-  operatorsAliases: false,
 
-  pool: {
-    max: env.max,
-    min: env.pool.min,
-    acquire: env.pool.acquire,
-    idle: env.pool.idle
-  }
-});
+if (process.env.JAWSDB_URL){
+  const sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+  const sequelize = new Sequelize(env.database, env.username, env.password, {
+    host: env.host,
+    dialect: env.dialect,
+    operatorsAliases: false,
+  
+    pool: {
+      max: env.max,
+      min: env.pool.min,
+      acquire: env.pool.acquire,
+      idle: env.pool.idle
+    }
+  });
+}
 
 const db = {};
 
